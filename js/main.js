367624f02d4c6c6fc60a36739276e2e9ef23398e
truongloc635 <truongquocloc635@gmail.com>
2025-04-19 20:50:54 +0700

    done header with responsive

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 /*Main java */
+function searchclick()
+{
+	if (window.matchMedia("(max-width: 800px)").matches) 
+	{
+		var x=document.getElementById("main-header");
+		x.style.display="none";
+		var y=document.getElementById("header-mid-min");
+		y.style.display="flex";
+		
+	}
+}	
+function hidesearchclick()
+{
+	if (window.matchMedia("(max-width: 800px)").matches) 
+	{
+		var y=document.getElementById("header-mid-min");
+		y.style.display="none";
+		var x=document.getElementById("main-header");
+		x.style.display="flex";		
+	}
+}
